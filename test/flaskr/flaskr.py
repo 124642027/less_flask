18b4ae9b5500803249524e210b6b145f0e981f63
lihaitao <lihaitao@zhangyue.com>
2015-06-03 11:50:13 +0800

    了解flask中通用视图的使用，http的restfull api的基础

--- test/flaskr/flaskr.py
+++ test/flaskr/flaskr.py
@@ -73,11 +73,78 @@ def teardown_request(exception):
         db.close()
     g.db.close()
 
-@app.route('/')
-def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+# @app.route('/')
+# def show_entries():
+#     cur = g.db.execute('select title, text from entries order by id desc')
+#     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+#     return render_template('show_entries.html', entries=entries)
+
+from flask.views import View
+
+
+class ShowEntries(View):
+    """通用视图的例子,将视图方法的各个部分拆开，
+    最主要的是dispatch_request和app.add_url_rule
+    """
+    methods = ['GET', 'POST']
+
+    @property
+    def get_template_name(self):
+        """获取模块名称"""
+        return 'show_entries.html'
+
+    def render_template(self, context):
+        """将context载入模板"""
+        return render_template(self.get_template_name, **context)
+
+    def get_objects(self):
+        """获取数据表中数据"""
+        cur = g.db.execute('select title, text from entries order by id desc')
+        return [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+
+    def dispatch_request(self):
+        """将封装数据，并载入模板"""
+        if request.method in ['POST', 'GET']:
+            context = {'entries': self.get_objects()}
+            return self.render_template(context)
+
+# 动态的添加handler入口，和@router效果一致
+app.add_url_rule('/', view_func=ShowEntries.as_view('show_entries'))
+
+# 基于http方法(GET/POST)的调度,将http请求的方法和类中定义的方法匹配，做对应的动作，restfull api
+'''
+class UserAPI(MethodView):
+
+    def get(self, user_id):
+        if user_id is None:
+            # 返回一个包含所有用户的列表
+            pass
+        else:
+            # 显示一个用户
+            pass
+
+    def post(self):
+        # 创建一个新用户
+        pass
+
+    def delete(self, user_id):
+        # 删除一个用户
+        pass
+
+    def put(self, user_id):
+        # update a single user
+        pass
+
+user_view = UserAPI.as_view('user_api')
+app.add_url_rule('/users/', defaults={'user_id': None},
+                 view_func=user_view, methods=['GET',])
+app.add_url_rule('/users/', view_func=user_view, methods=['POST',])
+app.add_url_rule('/users/<int:user_id>', view_func=user_view,
+                 methods=['GET', 'PUT', 'DELETE'])
+# default是参数的默认值
+# methods是http请求的方法
+# <int:user_id>指的是入参
+'''
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -124,25 +191,25 @@ from contextlib import contextmanager
 #     print 'Template %s is rendered with %s' % (template.name, context)
 
 # 信号使用的原始方式
-@contextmanager
-def captured_templates(app):
-    recorded = []
-    def record(sender, template, context, **extra):
-        recorded.append((template, context))
-    template_rendered.connect(record, app)
-    try:
-        yield recorded
-    finally:
-        template_rendered.disconnect(record, app)
-
-with captured_templates(app) as templates:
-    rv = app.test_client().get('/')
-    assert rv.status_code == 200
-    assert len(templates) == 1
-    template, context = templates[0]
-    print template, context
-    assert template.name == 'show_entries.html'
-    assert len(context['entries']) > 0
+# @contextmanager
+# def captured_templates(app):
+#     recorded = []
+#     def record(sender, template, context, **extra):
+#         recorded.append((template, context))
+#     template_rendered.connect(record, app)
+#     try:
+#         yield recorded
+#     finally:
+#         template_rendered.disconnect(record, app)
+#
+# with captured_templates(app) as templates:
+#     rv = app.test_client().get('/')
+#     assert rv.status_code == 200
+#     assert len(templates) == 1
+#     template, context = templates[0]
+#     print template, context
+#     assert template.name == 'show_entries.html'
+#     assert len(context['entries']) > 0
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8683, debug=False)
