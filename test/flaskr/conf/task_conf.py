cb63faf207ed0b59c8b5b6b2c2b0cfcfeaabba5f
lihaitao <lihaitao@zhangyue.com>
2015-06-16 17:53:22 +0800

    修改celery定时任务注释

--- test/flaskr/conf/task_conf.py
+++ test/flaskr/conf/task_conf.py
@@ -3,7 +3,7 @@ from celery.schedules import crontab
 CELERYBEAT_SCHEDULE = {
     'add-every-monday-morning': {
         'task': 'time_work.hello',
-        'schedule': crontab(),
+        'schedule': crontab(minute='50,51,52'),
         'args': (16, 16),
     },
 }
